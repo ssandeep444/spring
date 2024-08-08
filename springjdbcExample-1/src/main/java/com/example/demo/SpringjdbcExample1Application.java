9c3bdb9cc7c9a773541c6810a02b62965c0bf14a
Praanjal Jain <ssandeep444@gmail.com>
2024-08-08 23:02:05 +0530

    1st sringbt

---
+++ springjdbcExample-1/src/main/java/com/example/demo/SpringjdbcExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjdbcExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjdbcExample1Application.class, args);
+	}
+
+}
